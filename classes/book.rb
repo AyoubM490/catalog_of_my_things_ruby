e6ba8fd5f230cc21a3515bc5a3c6fc248825f8db
Ismail Courr <contact@ismailcourr.ml>
2022-06-27 19:22:46 +0100

    Create Book class Create Label class Create label.rb & book.rb files

---
+++ classes/book.rb
@@ -0,0 +1,15 @@
+require './item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publisher, cover_state, publish_date, archived: false, id: rand(1..2000))
+    @publisher = publisher
+    @cover_state = cover_state
+    super(publish_date, id: id, archived: archived)
+  end
+
+  def can_be_archived?
+    publish_date > 10
+  end
+end
