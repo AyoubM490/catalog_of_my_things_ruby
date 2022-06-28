4e665996e2cc259ac287e63fcf542f0f014be142
ayubch <ayoub.chahir96@gmail.com>
2022-06-28 16:54:32 +0100

    fix music_album unit test

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -2,12 +2,11 @@ $LOAD_PATH << '.'
 require_relative '../classes/genre'
 require_relative '../classes/item'
 
-
 describe Genre do
   context 'Given an item object' do
     it 'adds the item object to the items list' do
-      item = Item.new('11')
-      genre = Genre.new('comedy')
+      item = Item.new('thriller', '11')
+      genre = Genre.new('thriller')
 
       genre.add_item(item)
 
