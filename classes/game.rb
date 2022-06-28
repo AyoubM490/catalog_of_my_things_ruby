3aea81602f4d8f2591e0cbd4dc9f88457163e753
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-28 13:35:51 +0500

    update can_be_archived method

--- classes/game.rb
+++ classes/game.rb
@@ -1,8 +1,15 @@
+require 'date'
+
 class Game
   def initialize(multiplayer, date)
     @multiplayer = multiplayer
     @last_played_at = date
   end
 
-  def can_be_archived?; end
+  def can_be_archived?
+    last_played_at = Date.today.year - @last_played_at.year
+    return true if last_played_at > 2
+
+    false
+  end
 end
