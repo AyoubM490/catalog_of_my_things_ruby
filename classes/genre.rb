99d3a3ac66bf4292b2841ecd2d25375e3015dd9c
ayubch <ayoub.chahir96@gmail.com>
2022-06-28 15:48:22 +0100

    add genre class

---
+++ classes/genre.rb
@@ -0,0 +1,28 @@
+$LOAD_PATH << '.'
+require_relative './item'
+
+class Genre
+  attr_reader :id
+  attr_accessor :name
+  
+  def initialize(name)
+    @id = rand(1..1000)
+    @name = name
+    @items = []
+    @genres = []
+  end
+
+  def items
+    @items
+  end
+
+  def add_genre(genre)
+    @genres.push(genre) unless @genres.include?(genre)
+    genre.item(self)
+  end
+
+  def add_item(item)
+    @items.push(item) unless @items.include?(item)
+    item.genre = self
+  end
+end
