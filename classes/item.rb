a7490fe86f5c2a41ccd0b06b4b7b659fedeab90a
ayubch <ayoub.chahir96@gmail.com>
2022-06-28 15:48:38 +0100

    add item class

--- classes/item.rb
+++ classes/item.rb
@@ -1,17 +1,37 @@
 class Item
-  def initialize(publish_date, id: rand(1..2000), archived: true)
+  def initialize(genre, publish_date, id: rand(1..2000), archived: true)
     @id = id
     @archived = archived
+    @genre = genre
     @publish_date = publish_date
+    @archives = []
   end
 
   attr_accessor :id, :genre, :author, :label, :publish_date
 
+  # private
+
   def can_be_archived?()
-    @publish_date > 10
+    Integer(@publish_date) > 10
   end
 
-  def move_to_archive()
-    @archived = true if can_be_archived?
+  def move_to_archive(musicAlbum)
+    @archives.push(musicAlbum) unless @archives.include?(musicAlbum)
+  end
+
+  def list_music_albums()
+    @archives
+  end
+  
+  def genre
+    @genre
+  end
+
+  def list_genres()
+    genres = []
+    @archives.each do |archive|
+      genres.push(archive.genre)
+    end
+    genres
   end
 end
