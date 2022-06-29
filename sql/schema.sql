a35659903df2d1f8b7971cc90602437081be2f16
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-29 07:34:19 +0500

    add schema fjr author and game

---
+++ sql/schema.sql
@@ -0,0 +1,16 @@
+-- create author table
+CREATE TABLE author (
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL  
+);
+
+-- create games table
+CREATE TABLE game (
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  publish_date DATE,
+  archived BOOLEAN,
+  multiplayer BOOLEAN,  
+  last_played_at DATE,  
+  FOREIGN KEY (id) REFERENCES item (id) ON DELETE CASCADE  
+);
