c7ba0eb16c4bc39a1f23f0c14d04010ba14ba43a
Ismail Courr <contact@ismailcourr.ml>
2022-06-29 10:17:12 +0100

    Modify list books output

--- classes/book_options.rb
+++ classes/book_options.rb
@@ -1,9 +1,14 @@
 # Create a book.
 require_relative './book'
 require_relative './author'
+require_relative './label'
 
   # Create a book.
   def add_book
+    print 'Title:  '
+    title = gets.chomp
+    print 'Label color:  '
+    color = gets.chomp
     print 'Author first name:  '
     first_name = gets.chomp
     print 'Author last name:  '
@@ -15,8 +20,10 @@ require_relative './author'
     print 'Publish Date (Year):  '
     publish_date = gets.chomp
     author = Author.new(first_name, last_name)
+    label = Label.new(title, color)
     new_book = Book.new(publisher, cover_state, publish_date)
     new_book.author = author
+    new_book.label = label
 
     @books.push(new_book)
     puts
@@ -27,6 +34,6 @@ require_relative './author'
   end
 
   def list_all_books
-    @books.each_with_index { |book, index| puts "#{index + 1}) first name: #{book.author.first_name}" }
+    @books.each_with_index { |book, index| puts "#{index + 1}) \"#{book.label.title}\" by #{book.author.first_name}, #{book.author.last_name}" }
     puts
   end
