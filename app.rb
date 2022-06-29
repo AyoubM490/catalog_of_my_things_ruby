cdc0771611c79ca58b73cca03c177be39be3d68f
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-29 13:46:58 +0500

    update create table game

--- app.rb
+++ app.rb
@@ -1,3 +1,8 @@
+require 'json'
+require_relative './classes/author'
+require_relative './classes/game'
+
+
 class App
   def initialize
     @options = [
@@ -12,17 +17,32 @@ class App
       'Add a game',
       'Exit'
     ]
+
+    @music_albums = MusicAlbum.read_file(@genres)
+    @games = []
+    @authors = []
+    add_authors
   end
+
+  def add_authors
+    @author_names = ['Stephen King', 'Hermann Hesse', 'J.K Rowling', 'Friedrich Nietzsche']
+    @author_names.each do |author|
+      first_name = author.split[0]
+      last_name = author.split[1]
+      @authors << Author.new(first_name, last_name)
+    end
+  end
+  
 
   def run
     @options.each_with_index do |option, index|
-      puts "[#{index + 1}] - #{option}"
+      puts "[#{index + 1}]- #{option}"
     end
     user_input
   end
 
   def user_input
-    puts 'Please choose an option [1-10] '
+    print 'Please choose an option [1-10] '
     user_choice = gets.chomp.to_i
     operations(user_choice)
   end
@@ -51,5 +71,43 @@ class App
 
   def exit
     p 'Thank you for using this app. Have a good day!'
+  end
+
+  def list_all_authors
+    p 'There are no authors here' if @authors.length.zero?
+    @authors.each_with_index do |author, index|
+      p "[#{index}] - #{author.first_name} #{author.last_name}"
+    end
+    puts
+  end
+
+  def add_game
+    print 'Is this game for multiplayers? [Y/N]'
+    multiplayer = gets.chomp.downcase
+    multiplayer = multiplayer == 'y'
+    print 'Please enter the year this game was last played in [YYYY]: '
+    last_played_at = gets.chomp.to_i
+    print 'Please enter the game published year [YYYY]: '
+    publish_date = gets.chomp.to_i
+    print 'Has the game been archived [Y/N]: '
+    archived = gets.chomp.downcase
+    archived = archived == 'y'
+    puts 'Choose an author from the following list using the number'
+    list_all_authors
+    author_chosen = gets.chomp.to_i
+    game = Game.new(multiplayer, last_played_at, publish_date, archived)
+    game.add_author(@authors[author_chosen])
+    @games << game
+    p 'The game has been added successfully!'
+    puts
+  end
+
+  def list_all_games
+    p 'There are no games here!' if @games.length.zero?
+    @games.each_with_index do |game, index|
+      p "[#{index + 1}] - Multiplayer: #{game.multiplayer},
+      Last Played: #{game.last_played_at}, Archived: #{game.archived}"
+    end
+    puts
   end
 end
